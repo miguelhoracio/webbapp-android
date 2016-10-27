7ee90b6fba75b9a7e6ba0609b670a1f1c7e1601f
miguelhoracio <miguel_horaacio@hotmail.com>
2016-10-26 20:56:36 -0500

    Create Confusion.java

---
+++ Confusion.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class Confusion{
+    public static void main(String[] args) {
+        String;
+        System.out.println("¿Que tan confundido estas con este proyecto?");
+       
+        
+        if("Eres miguel?"=="Si")
+			System.out.println("Estas completamente confundido y perdido");
+		
+		if("Eres cheko?"=="Si")
+			System.out.println("Conoces a la perfeccion los pŕogramas porque tu los hiciste");
+		
+		/*¿Hey enserio como se supone que entienda los codigos y que hacen?
+		  Asi no puedo contribuir, porfa mejor dime que hacer cheko.
+		  Atte: Miguel*/
+	
+    }
+}
